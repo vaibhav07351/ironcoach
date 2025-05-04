de3c1a727e3c75d4c23f9381772147afb2c65149
Vaibhav Bhardwaj <vaibhav07351@gmail.com>
2025-05-05 01:53:21 +0530

    cors response include in middleware

--- backend/middlewares/auth.go
+++ backend/middlewares/auth.go
@@ -7,26 +7,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AuthMiddleware() gin.HandlerFunc{
-	return func(c *gin.Context){
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Set CORS headers in case of abort
+		origin := c.Request.Header.Get("Origin")
+		if origin == "https://ironcoach--ctsjgkrhrb.expo.app" ||
+			origin == "https://ironcoach--ironcoach-staging.expo.app" ||
+			origin == "https://ironcoach.expo.app" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		}
+
 		// Get the token from the Authorization header
 		tokenString := c.GetHeader("Authorization")
-		if tokenString == ""{
-			c.JSON(http.StatusUnauthorized, gin.H{"error":"Authorization token required"})
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token required"})
 			c.Abort()
 			return
 		}
 
 		//verify the token
 		claims, err := utils.VerifyJWT(tokenString)
-		if err!=nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error":"invalid or expired token"})
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired token"})
 			c.Abort()
 			return
 		}
 
-		c.Set("email",claims.Email)
+		c.Set("email", claims.Email)
 		c.Next()
-
 	}
 }
