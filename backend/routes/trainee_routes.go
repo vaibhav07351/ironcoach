6561d16116c341bbae76d593917ea7a177efc7ce
Vaibhav Bhardwaj <vaibhav07351@gmail.com>
2025-05-20 01:08:59 +0530

    trial backend change in trainee route

--- backend/routes/trainee_routes.go
+++ backend/routes/trainee_routes.go
@@ -11,6 +11,7 @@ func RegisterTraineeRoutes(router *gin.Engine) {
     protected := router.Group("/trainees").Use(middlewares.AuthMiddleware())
 
     protected.POST("/", traineeController.AddTrainee)
+    protected.GET("", traineeController.GetTrainees)
     protected.GET("/", traineeController.GetTrainees)
     protected.GET("/:trainee_id", traineeController.GetTraineeByID)
     protected.PUT("/:id", traineeController.UpdateTrainee)
